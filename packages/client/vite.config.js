ce0727852ed6b922a7f565d9f832fc937fc192ac
Volodymyr <vderunov@gmail.com>
2024-01-22 21:43:42 +0200

    Update base URL in vite config

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "pizza_order_web3",
+  base: "/pizza_order_web3/",
   plugins: [react()],
   server: {
     port: 3000,
